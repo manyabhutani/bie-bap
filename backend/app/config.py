38ab7c7ef70601752ffdaf5fe5e21904aab4476c
bhutaman <bhutaman@fit.cvut.cz>
2025-04-25 12:34:29 +0200

    docker

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,20 +1,11 @@
 import os
-from dotenv import load_dotenv
-if os.environ.get("RENDER") != "true":
-    load_dotenv("app/.env")
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 class Settings:
     access_token_expire_minutes: int = 15
-    refresh_token_expire_minutes: int = 60 * 24 * 7  # 7 days
+    refresh_token_expire_minutes: int = 60 * 24 * 7
     algorithm: str = "HS256"
 
-    class Config:
-        env_file = "app/.env"
-
 settings = Settings()
-
-
-
