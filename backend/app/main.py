d24c1712e8b623fecee19be8c1433bba3f6a02ab
bhutaman <bhutaman@fit.cvut.cz>
2025-04-27 16:10:35 +0200

    render backend connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="Volunteer Management API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000" , 'https://bie-bap.onrender.com'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
