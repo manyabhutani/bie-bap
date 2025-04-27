a495c52c964d838dce9198fd903120be49bd5913
bhutaman <bhutaman@fit.cvut.cz>
2025-04-27 15:47:03 +0200

    render backend connection

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //docker
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+const API_URL = 'https://volunteer-backend-xwn1.onrender.com';
 
 const API = axios.create({
     baseURL: API_URL,
