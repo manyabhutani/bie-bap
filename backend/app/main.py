0630e1b60fb3bc8dd007f2a8f93351600bffd432
bhutaman <bhutaman@fit.cvut.cz>
2025-04-27 17:38:57 +0200

    render backend connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,14 @@
 from fastapi import FastAPI
 from app.db.session import engine, Base
-from app.api.router import auth, volunteers, events , organizers
+from app.api.router import auth, volunteers, events, organizers
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Volunteer Management API")
 
-#for render
+# for render
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000",
-                  "https://volunteer-management-ca2l.onrender.com"],
+    allow_origins=["https://volunteer-management-ca2l.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -18,7 +17,7 @@ Base.metadata.create_all(bind=engine)
 
 app.router.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.router.include_router(volunteers.router, prefix="/volunteers", tags=["volunteers"])
-app.router.include_router(organizers.router , prefix="/organizers" , tags=["organizers"])
+app.router.include_router(organizers.router, prefix="/organizers", tags=["organizers"])
 app.router.include_router(events.router, prefix="/events", tags=["events"])
 
 
